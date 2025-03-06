e45a790c5504ba21099bb5973174e4e8ab9d1d07
hajun <inhj@thenextdoor.co.kr>
2025-03-06 13:02:54 +0900

    1차 커밋

---
+++ android/app/src/main/kotlin/com/example/android_web_view_check/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.example.android_web_view_check
+
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugin.common.MethodChannel
+import android.webkit.WebView
+import io.flutter.embedding.android.FlutterFragmentActivity
+
+class MainActivity : FlutterFragmentActivity() {
+    private val CHANNEL = "android_webview_version"
+
+    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
+        super.configureFlutterEngine(flutterEngine)
+
+        MethodChannel(flutterEngine.dartExecutor.binaryMessenger, CHANNEL).setMethodCallHandler { call, result ->
+            if (call.method == "getWebViewVersion") {
+                val webViewVersion = WebView(this).settings.userAgentString
+                result.success(webViewVersion)
+            } else {
+                result.notImplemented()
+            }
+        }
+    }
+}
---
+++ lib/android_web_view_check.dart
@@ -0,0 +1,46 @@
+library AndroidWebViewCheck;
+
+import 'dart:io';
+
+import 'package:device_info_plus/device_info_plus.dart';
+import 'package:flutter/services.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+class AndroidWebViewCheck {
+  Future<Map> deviceCheck() async {
+    Map map = {};
+    if (Platform.isAndroid) {
+      final DeviceInfoPlugin deviceInfo = DeviceInfoPlugin();
+      // Android 디바이스 정보 가져오기
+      AndroidDeviceInfo androidInfo = await deviceInfo.androidInfo;
+
+      const platform = MethodChannel("android_webview_version");
+
+      try {
+        final String version = await platform.invokeMethod("getWebViewVersion");
+
+        // 정규식을 사용하여 WebView 버전만 추출
+        RegExp regExp = RegExp(r'Chrome/(\d+)\.');
+        Match? match = regExp.firstMatch(version);
+
+        if (match != null) {
+          String majorVersion = match?.group(1) ?? "Unknown";
+          // 엘지폰이고 웹뷰구현 버전이 90대 이하면 (엘지 스마트폰 사업 철수로 인한 대응)
+          return map = {"brand": androidInfo.brand, "update": int.parse(majorVersion) < 90 ? true : false, "msg": ""};
+        } else {
+          return map = {"brand": "", "update": false, "msg": "WebView version not found."};
+        }
+      } on PlatformException catch (e) {
+        print("Error  WebView version: '${e.message}'.");
+      }
+
+      void updateWebView() async {
+        final url = "https://play.google.com/store/apps/details?id=com.google.android.webview";
+        if (await canLaunch(url)) {
+          await launch(url);
+        }
+      }
+    }
+    return map;
+  }
+}
